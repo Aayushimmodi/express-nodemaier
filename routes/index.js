8349cec77294dac5de2019a5d033720db1fdae58
Aayushi Modi <modi.73aayushi@gmail.com>
2024-07-10 11:22:44 +0530

    add files

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/form', function(req, res, next) {
+  res.render('form');
+});
+router.post('/process-form', function(req, res, next) {
+  var name = req.body.name;
+  var email =  req.body.email;
+  var password =  req.body.password;
+  res.send('response',{myname : name, myemail:email,mypassword:password})
+});
 module.exports = router;
